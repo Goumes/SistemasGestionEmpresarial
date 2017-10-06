85ecce4d32bf389655368c715f0e94d65ef3f58e
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-10-06 12:46:50 +0200

    Ejercicios MVC

---
+++ 02-HelloWorld-MVC/02-HelloWorld-MVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02_HelloWorld_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return  ("Hello World from Controller");
+        }
+
+        public ActionResult Saludo()
+        {
+            return View();
+        }
+    }
+}
