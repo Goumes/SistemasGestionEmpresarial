bc20871d2596ed9e86dd9850334313f5a6b46637
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-11-16 19:58:23 +0100

    VIEWBAG.CRACK = "TRUE AS FUCK"
    Hechos los ejercicios atrasados de sistemas de gestión con asp.net y el MVC

---
+++ 07-ConexionBadatAzure-ASP/07-ConexionBadatAzure-ASP/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _07_ConexionBadatAzure_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(String abrir)
+        {
+            SqlConnection miConexion = new SqlConnection();
+
+            try
+            {
+                miConexion.ConnectionString = "server = personasbdserver.database.windows.net;database = PersonasDB; uid = agomez; pwd = Password123;";
+                miConexion.Open();
+            }
+            catch (ArgumentException e) { }
+
+            ViewBag.conexionAbierta = miConexion.State;
+            return View();
+        }
+
+    }
+}
---
+++ 08-ListadoPersonasAzure-ASP/08-ListadoPersonasAzure-ASP/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using _05_PersonaModificada_ASP.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _08_ListadoPersonasAzure_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            SqlConnection miConexion = new SqlConnection();
+
+            List<clsPersona> listadoPersonas = new List<clsPersona>();
+
+            SqlCommand miComando = new SqlCommand();
+
+            SqlDataReader miLector;
+
+            clsPersona oPersona;
+
+
+            miConexion.ConnectionString = "server = personasbdserver.database.windows.net;database = PersonasDB; uid = agomez; pwd = Password123;";
+
+
+            try
+            {
+                miConexion.Open();
+
+                //Creamos el comando (Creamos el comando, le pasamos la sentencia y la conexion, y lo ejecutamos)
+
+                miComando.CommandText = "SELECT * FROM personas";
+                
+                miComando.Connection = miConexion;
+                miLector = miComando.ExecuteReader();
+
+                //Si hay lineas en el lector
+                if (miLector.HasRows)
+                {
+                    while (miLector.Read())
+                    {
+                        oPersona = new clsPersona();
+                        oPersona.idPersona = (int)miLector["IDPersona"];
+                        oPersona.nombre = (string)miLector["nombre"];
+                        oPersona.apellidos = (string)miLector["apellidos"];
+                        oPersona.fechaNac = (DateTime)miLector["fechaNac"];
+                        oPersona.direccion = (string)miLector["direccion"];
+                        oPersona.telefono = (string)miLector["telefono"];
+                        listadoPersonas.Add(oPersona);
+                    }
+                }
+
+                miLector.Close();
+                miConexion.Close();
+            }
+
+            catch (SqlException exSql)
+            {
+                throw exSql;
+            }
+
+            return View(listadoPersonas);
+        }
+    }
+}
