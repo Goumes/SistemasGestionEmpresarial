531ee323c9c1f5ff09162e9b4f6b893f3a84b804
Alejandro Gómez Olivera <alejandro_golivera@hotmail.com>
2017-11-17 10:25:30 +0100

    Small change hehexd :mag:

--- 07-ConexionBadatAzure-ASP/07-ConexionBadatAzure-ASP/Controllers/HomeController.cs
+++ 07-ConexionBadatAzure-ASP/07-ConexionBadatAzure-ASP/Controllers/HomeController.cs
@@ -16,7 +16,8 @@ namespace _07_ConexionBadatAzure_ASP.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(String abrir)
+        [ActionName ("Index")]
+        public ActionResult IndexPost()
         {
             SqlConnection miConexion = new SqlConnection();
 
@@ -24,10 +25,14 @@ namespace _07_ConexionBadatAzure_ASP.Controllers
             {
                 miConexion.ConnectionString = "server = personasbdserver.database.windows.net;database = PersonasDB; uid = agomez; pwd = Password123;";
                 miConexion.Open();
+                ViewBag.conexionAbierta = miConexion.State;
             }
-            catch (ArgumentException e) { }
+            catch (ArgumentException e)
+            {
+                ViewBag.conexionAbierta = "La has liao paco";
+            }
 
-            ViewBag.conexionAbierta = miConexion.State;
+            
             return View();
         }
 
